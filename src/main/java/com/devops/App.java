1538e1b58882ffc0a75031c90cb2e7e8701f9a3e
Varun210706 <avisinedivarun2006@gmail.com>
2026-03-08 02:09:15 +0530

    Implement HTTP server in App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,15 +1,25 @@
 package com.devops;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Application started!");
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        while(true){
-            System.out.println("Running in Kubernetes...");
-            Thread.sleep(5000);
-        }
+        server.createContext("/", exchange -> {
+            String response = "Java App Running in Kubernetes!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
 
+        server.start();
+
+        System.out.println("Server started on port 8080");
     }
 }
